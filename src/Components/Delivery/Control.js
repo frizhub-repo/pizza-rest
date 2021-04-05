1d5d1c1ca69c6cdaee0ed42de5a2875248ef10eb
huzaifa1214 <huzaifaimran1998@gmail.com>
2021-04-05 20:38:03 +0500

    payment integrated

--- src/Components/Delivery/Control.js
+++ src/Components/Delivery/Control.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Spinner } from "react-bootstrap";
 import axiosIntance from "../../axios-configured";
@@ -9,6 +9,7 @@ import AuthModal from "../Auth/AuthModal";
 import { createOrder } from "../../api/orders";
 import { useHistory } from "react-router";
 import CompleteOrderModal from "../CustomComponents/CompleteOrderModal";
+import { Card } from "@material-ui/core";
 
 function Control() {
   let { token } = useRestaurantConetxt();
@@ -24,6 +25,10 @@ function Control() {
   const handleClose = () => setShowModal(false);
   const handleShow = () => setShowModal(true);
   const disp = useDispatch();
+  const [showBtn, setShowBtn] = useState(false);
+  const [status, setStatus] = useState(null);
+
+  const paypal = useRef();
 
   const orderNow = async () => {
     products.length > 0 && history.push("/complete/purchase");
@@ -39,6 +44,105 @@ function Control() {
     //   console.log(err);
     // }
   };
+
+  // const createOrder = () => {
+  //   // if (status === "ERROR") {
+  //   //   toast.error("Payment cancel for some reason. Please provide payment");
+  //   // } else if (status === "COMPLETED") {
+  //   //   setStatus(null);
+  //   //   axiosIntance
+  //   //     .post("/api/v1/orders/customers", { products: items })
+  //   //     .then((res) => {
+  //   //       toast.success("Order created successfully");
+  //   //       disp(removeOrderItems());
+  //   //       history.push("/");
+  //   //       console.log(res);
+  //   //     })
+  //   //     .catch((err) => console.log(err));
+  //   // } else {
+  //   //   toast.error("Payment not added. Please provide payment");
+  //   // }
+  //   axiosIntance
+  //     .post("/api/v1/orders/customers", { products })
+  //     .then((res) => {
+  //       toast.success("Order created successfully");
+  //       setShowBtn(false);
+  //       disp(removeOrderItems());
+  //       // history.push("/");
+  //       console.log(res);
+  //     })
+  //     .catch((err) => console.log(err));
+  // };
+
+  // useEffect(() => {
+  //   var FUNDING_SOURCES = [
+  //     window.paypal.FUNDING.PAYPAL,
+  //     window.paypal.FUNDING.PAYLATER,
+  //     window.paypal.FUNDING.CREDIT,
+  //     window.paypal.FUNDING.CARD,
+  //   ];
+
+  //   FUNDING_SOURCES.forEach(function (fundingSource) {
+  //     // Initialize the buttons
+  //     var button = window.paypal.Buttons({
+  //       style: {
+  //         shape: "pill",
+  //         layout: "horizontal",
+  //         margin: "20px",
+  //       },
+  //       fundingSource: fundingSource,
+  //       createOrder: (data, actions, err) => {
+  //         return actions.order.create({
+  //           intent: "CAPTURE",
+  //           purchase_units: [
+  //             {
+  //               description: "Restaurant Club",
+  //               amount: {
+  //                 value: total,
+  //               },
+  //             },
+  //           ],
+  //         });
+  //       },
+  //       onApprove: async (data, actions) => {
+  //         const order = await actions.order.capture();
+  //         if (order?.status === "COMPLETED") {
+  //           // toast.success("Payment got successfull");
+  //           setStatus("COMPLETED");
+  //           createOrder();
+  //         } else {
+  //           setStatus("ERROR");
+  //           toast.error("Something went wrong");
+  //         }
+  //         console.log({ order });
+  //       },
+  //       onError: (err) => {
+  //         setStatus("ERROR");
+  //         toast.error("Error occured while sending money");
+  //         console.log({ err });
+  //       },
+  //       onCancel: (data) => {
+  //         setStatus("ERROR");
+  //         toast.error("Payment cancel by user");
+  //         console.log({ data });
+  //       },
+  //     });
+
+  //     // Check if the button is eligible
+  //     if (button.isEligible()) {
+  //       // Render the standalone button for that funding source
+  //       button.render(paypal.current);
+  //     }
+  //   });
+  //   // Add style on paypal buttons at run time
+  //   let content = document.getElementsByClassName(
+  //     "paypal-buttons-layout-horizontal"
+  //   );
+  //   for (let i = 0; i < content.length; i++) {
+  //     content[i].style.margin = "5px";
+  //     content[i].style.width = "100%";
+  //   }
+  // }, []);
 
   return (
     <div className="w-full ml-4 p-1">
@@ -135,6 +239,7 @@ function Control() {
           </p>
         </div>
       </section>
+
       <AuthModal show={modalShow} onHide={() => setModalShow(false)} />
       <CompleteOrderModal show={showModal} handleClose={handleClose} />
     </div>
