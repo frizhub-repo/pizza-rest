fd65689e4420d1f728d4d36ecc350953739c4b41
nauman306 <m.nauman@frizhub.com>
2021-04-19 17:08:39 +0500

    Get Opening hours from google my business and show in contact us page

--- src/api/public.js
+++ src/api/public.js
@@ -3,3 +3,7 @@ import axiosIntance from "../axios-configured";
 export const productsByCategory = () => {
   return axiosIntance.get("/api/v1/products/category/public");
 };
+
+export const getGoogleMyBusinessLocations = () => {
+  return axiosIntance.get("/api/v1/my_business/locations/public");
+};
