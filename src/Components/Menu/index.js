f916bb91ce017f705b43861137ee0e9cf2d0f867
Dawood-Javed <dawood.javed@frizhub.com>
2021-07-13 21:24:43 +0500

    menu page done 70 percent and adding cards to all other page to hero

--- src/Components/Menu/index.js
+++ src/Components/Menu/index.js
@@ -5,21 +5,32 @@ import Carousel from "react-multi-carousel";
 import Card from "./Card";
 import food from "../../images/food.svg";
 
-import { useDispatch, useSelector } from "react-redux";
 import { productsByCategory, customerMenu } from "../../api/public";
 import { useAccordionToggle } from "react-bootstrap/AccordionToggle";
-import { GET_PRODUCTS_BY_CATEGORY } from "../../utils/types";
 import Skeleton from "@material-ui/lab/Skeleton";
 import Hero from "../Home/Hero.js";
+import ItemCard from "../itemCard/index";
+import { makeStyles } from "@material-ui/core/styles";
+import ButtonCard from "../Home/buttonCard";
+import MenuCard from "../Home/MenuCard";
+
+const styles = makeStyles({
+  container: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+  },
+  hrStyles: {
+    color: "black",
+  },
+});
 
 function Menu() {
-  const dispatch = useDispatch();
+  const classes = styles();
   const [loading, setLoading] = useState(false);
   const [key, setKey] = useState(0);
   const [menus, setMenus] = useState([]);
-  const { productsByCategory: products } = useSelector(
-    (state) => state.products
-  );
+
   const fetchProductsByCategory = async () => {
     try {
       setLoading(true);
@@ -34,13 +45,52 @@ function Menu() {
   useEffect(() => {
     fetchProductsByCategory();
   }, []);
+  const url2 =
+    "http://hobokenmaddhatter.com/wp-content/uploads/2019/02/NEW-LUNCH-MENU.revised-01.png";
   const url =
     "https://images.unsplash.com/photo-1562059390-a761a084768e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1906&q=80";
   return (
     <div>
       <Navbar />
       <Hero textOne="Uncle Sammy" textTwo="Menu Selection!" url={url} />
+      <div className={classes.container}>
+        <ItemCard
+          key={1}
+          title={"LUNCH"}
+          image={url2}
+          width="300px"
+          margin="20px"
+        />
 
+        <ItemCard
+          key={1}
+          title={"DINNER"}
+          image={url2}
+          width="300px"
+          margin="20px"
+        />
+        <ItemCard
+          key={1}
+          title={"DESSERTS"}
+          image={url2}
+          width="300px"
+          margin="20px"
+        />
+        <ItemCard
+          key={1}
+          title={"BRUNCH"}
+          image={url2}
+          width="300px"
+          margin="20px"
+        />
+      </div>
+      <div className={classes.container}>
+        <ButtonCard text="RESERVE A TABLE" />
+      </div>
+
+      <div className={classes.container}>
+        <MenuCard text="RESERVE A TABLE" />
+      </div>
       <div className="w-full ">
         <Carousel
           additionalTransfrom={0}
@@ -104,83 +154,9 @@ function Menu() {
               icon={food}
             />
           ))}
-          {/* <Card
-            text={
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, \n" +
-              "pulvinar facilisis justo mollis"
-            }
-            title={"Dinner"}
-            image={
-              "https://images.unsplash.com/photo-1542834369-f10ebf06d3e0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=3150&q=80"
-            }
-            icon={dish}
-          />
-          <Card
-            text={
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, \n" +
-              "pulvinar facilisis justo mollis"
-            }
-            title={"Desserts"}
-            image={
-              "https://images.unsplash.com/photo-1542834369-f10ebf06d3e0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=3150&q=80"
-            }
-            icon={cupcake}
-          /> */}
         </Carousel>
       </div>
 
-      {/* <div className="w-full mt-8 flex px-44">
-                <div className="w-1/2 px-24">
-                    {products?.length
-                        ? products?.map((product) => (
-                              <div className="w-full">
-                                  <h1 className="text-4xl font-weight-bold font-italic text-yellow-400 text-left mb-4">
-                                      {product?.name}
-                                  </h1>
-                                  {product?.products?.length
-                                      ? product?.products?.map((item) => (
-                                            <p className="text-lg text-left mb-8 font-italic">
-                                                {item.title}
-                                            </p>
-                                        ))
-                                      : null}
-                              </div>
-                          ))
-                        : null}
-                </div>
-            </div> */}
-      {/* <Container>
-                <Row className="w-full mt-8 flex px-44">
-                    {products?.length
-                        ? products?.map((product) => (
-                              <Col md={6}>
-                                  <div className="w-full">
-                                      <h1 className="text-4xl font-weight-bold font-italic text-yellow-400 text-left mb-4">
-                                          {product?.name}
-                                      </h1>
-                                      {product?.products?.length
-                                          ? product?.products?.map((item) => (
-                                                <p className="text-lg text-left mb-8 font-italic">
-                                                    {item.title}{" "}
-                                                    <label
-                                                        style={{
-                                                            marginLeft: "10px",
-                                                        }}
-                                                    >
-                                                        {item.price}{" "}
-                                                        {item.currency}
-                                                    </label>
-                                                </p>
-                                            ))
-                                          : null}
-                                  </div>
-                              </Col>
-                          ))
-                        : null}
-                </Row>
-            </Container> */}
-
-      {/* <Container> */}
       <div
         className="w-full mt-8 flex px-44"
         style={{ display: "flex", flexWrap: "wrap" }}
