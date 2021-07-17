4726bc1b35579bba0a79674807e4ef71fd348b78
Farhan Saqib <developer.farhansaqib@gmail.com>
2021-07-17 16:21:14 +0500

    section 3

--- src/Components/Home/section3.js
+++ src/Components/Home/section3.js
@@ -1,46 +1,58 @@
 import React from "react";
-import delivery from "../../images/delivery.png";
-import rating from "../../images/rating.png";
-import map from "../../images/map.png";
-import cost from "../../images/cost.png";
-function Section3() {
+import { Card, CardContent } from "@material-ui/core";
+import dish from "../../images/dish.png";
+import carousel from "../../images/carousel.png";
+import roundImage from "../../images/roundImage.png";
+import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
+import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
+import { makeStyles } from "@material-ui/core";
+import styles from "./styles";
+
+const useStyles = makeStyles(styles);
+export default function Section3() {
+  const classes = useStyles();
   return (
-    <section className="flex py-20  w-full ">
-      <div className="flex-grow-1 w-5/6 h-full ml-12 mt-2">
-        <div className="h-24 flex ">
-          <div className="w-1/4  ">
-            <p className=" text-gray-500 text-center text-sm">Address</p>
-            <img className="object-contain mt-2 w-full h-16 " src={map} />
+    <div className={classes.s3MainDiv}>
+      <div className={classes.s3InnerDiv}>
+        <Card className={classes.s3FirstCard}>
+          <CardContent>
+            <div>
+              <img src={dish} className={classes.s3FirstCardImage} />
+            </div>
+          </CardContent>
+          <CardContent>
+            <img src={carousel} />
+          </CardContent>
+        </Card>
 
-            <p className=" text-gray-500 text-center mt-2 text-sm">
-              Via Cristoforo Colombo,44
-            </p>
-          </div>
+        <div className={classes.carouselDiv}>
+          <Card className={classes.carouselLeftCard}>
+            <ArrowBackIosIcon />
+            <ArrowBackIosIcon />
+          </Card>
 
-          <div className="w-1/4  ">
-            <p className=" text-gray-500 text-center text-sm">Delivery</p>
-            <img className="h-16 object-contain  w-full" src={delivery} />
-
-            <p className=" text-gray-500 text-center mt-2 text-sm">
-              Minimum Order 15$
-            </p>
-          </div>
-          <div className="w-1/4  ">
-            <p className=" text-gray-500 text-center text-sm">Delivery Fee</p>
-            <img className="object-contain  w-full h-16 " src={cost} />
-            <p className=" text-gray-500 text-center mt-2 text-sm">
-              Minimum Order 15$
-            </p>
-          </div>
-          <div className="w-1/4  ">
-            <p className=" text-gray-500 text-center text-sm">Reviews</p>
-            <img className="object-contain  w-full h-16" src={rating} />
-            <p className=" text-gray-500 text-center mt-2 text-sm">Satisfid</p>
-          </div>
+          <Card className={classes.carouselRightCard}>
+            <ArrowForwardIosIcon />
+            <ArrowForwardIosIcon />
+          </Card>
+          <Card className={classes.carouselInnerCard}>
+            <CardContent>
+              <div className={classes.carouselCardImageDiv}>
+                <img src={roundImage} className={classes.carouselCardImage} />
+              </div>
+              <div className={classes.whiteBar}></div>
+              <div>
+                <p className={classes.text}>
+                  ''E’ il mio ristorante preferito a Pisa. Andateci e non ve ne
+                  pentirete!!! I piatti sono buonissimi ed in più il personale è
+                  gentile. Super consigliato, noi appena possiamo (data la
+                  pandemia) ci torniamo con piacere'’
+                </p>
+              </div>
+            </CardContent>
+          </Card>
         </div>
       </div>
-    </section>
+    </div>
   );
 }
-
-export default Section3;
