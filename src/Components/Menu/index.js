d761dfbac0a94f12a2c1f6f1b60e6bdea201db71
naumanmallik <m.nauman@frizhub.com>
2021-08-24 21:13:53 +0500

    Change header images in all components

--- src/Components/Menu/index.js
+++ src/Components/Menu/index.js
@@ -3,31 +3,30 @@ import Navbar from "../Navbar";
 import Footer from "../Footer";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import { productsByCategory, customerMenu } from "../../api/public";
-import { useAccordionToggle } from "react-bootstrap/AccordionToggle";
+import { customerMenu } from "../../api/public";
 import Hero from "../Home/Hero.js";
 import ItemCard from "../itemCard/index";
 import { makeStyles } from "@material-ui/core/styles";
 import ButtonCard from "../Home/buttonCard";
 import MenuCard from "../Home/MenuCard";
-import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
-import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
 import { useRestaurantContext } from "../../Context/restaurantContext";
 import { Backdrop, CircularProgress } from "@material-ui/core";
+import menuHeaderImg from "Assets/images/menuHeader.png";
 
 const styles = makeStyles({
   container: {
     display: "flex",
-    flexWrap: "wrap",
+    flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
-    boxShadow: "0 4px 4px rgb(0 0 0 / 20%)",
-  },
-  container2: {
-    marginBottom: "80px",
+    padding: "20px 150px",
   },
   hrStyles: {
     color: "black",
+  },
+  spacing: {
+    marginTop: "30px",
+    width: "100%",
   },
   backdrop: {
     zIndex: 1,
@@ -57,7 +56,6 @@ function Menu() {
 
   const classes = styles();
   const [loading, setLoading] = useState(false);
-  const [key, setKey] = useState(0);
   const [menus, setMenus] = useState([]);
   const [selectedMenu, setSelectedMenu] = React.useState({});
 
@@ -86,17 +84,14 @@ function Menu() {
   useEffect(() => {
     fetchProductsByCategory();
   }, []);
-  const url2 =
-    "http://hobokenmaddhatter.com/wp-content/uploads/2019/02/NEW-LUNCH-MENU.revised-01.png";
-  const url =
-    "https://images.unsplash.com/photo-1562059390-a761a084768e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1906&q=80";
+
   return (
     <div>
       <Navbar />
       <Hero
         textOne={restaurant?.restaurant?.name ?? "Uncle Sammy"}
         textTwo="Menu Selection!"
-        url={url}
+        url={menuHeaderImg}
         restaurantLogo={restaurant?.restaurant?.logoUrl}
       />
       <div style={{ margin: "50px 0px" }}>
@@ -135,10 +130,9 @@ function Menu() {
 
       <div className={classes.container}>
         <ButtonCard text="RESERVE A TABLE" />
-      </div>
-
-      <div className={`${classes.container} ${classes.container2}`}>
-        <MenuCard selectedMenu={selectedMenu} />
+        <div className={classes.spacing}>
+          <MenuCard selectedMenu={selectedMenu} />
+        </div>
       </div>
 
       <Footer />
@@ -146,22 +140,6 @@ function Menu() {
         <CircularProgress color="inherit" />
       </Backdrop>
     </div>
-  );
-}
-
-function CustomToggle({ children, eventKey }) {
-  const decoratedOnClick = useAccordionToggle(eventKey, () =>
-    console.log("totally custom!")
-  );
-
-  return (
-    <button
-      type="button"
-      style={{ backgroundColor: "pink" }}
-      onClick={decoratedOnClick}
-    >
-      {children}
-    </button>
   );
 }
 
