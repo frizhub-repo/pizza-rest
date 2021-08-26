d6af8b6be18de7433e844c1a3dc9d460fee1f23e
naumanmallik <m.nauman@frizhub.com>
2021-08-26 16:59:28 +0500

    Google Map added

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -19,6 +19,7 @@ import MyCarousel from "react-multi-carousel";
 import DiscountCarousel from "Components/DiscountCarousel";
 import header1 from "Assets/images/header1.jpg";
 import { isEmpty } from "utils/common";
+import GoogleMap from "Components/CustomComponents/GoogleMap";
 
 const responsive = {
   desktop: {
@@ -240,17 +241,7 @@ function Home() {
             </div>
           </div>
           <div className={classes.googleMapRoot}>
-            <iframe
-              width="100%"
-              height="100%"
-              frameBorder="0"
-              title="map"
-              marginHeight="0"
-              marginWidth="0"
-              scrolling="no"
-              src="https://maps.google.com/maps?width=100%&height=600&hl=en&q=%C4%B0zmir+(My%20Business%20Name)&ie=UTF8&t=&z=14&iwloc=B&output=embed"
-              className={classes.googleMap}
-            ></iframe>
+            <GoogleMap classname={classes.googleMap} />
           </div>
         </div>
       ) : null}
