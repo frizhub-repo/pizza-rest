4a0751cf7c582a11b9c31a08e30f4a608b2d373f
naumanmallik <m.nauman@frizhub.com>
2021-08-26 14:32:57 +0500

    Show google places opening hour and address data in Home Page

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -12,13 +12,13 @@ import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import SectionThree from "./SectionThree";
-import { getGoogleMyBusinessLocations } from "../../api/public";
 import { useRestaurantContext } from "../../Context/restaurantContext";
 import Skeleton from "@material-ui/lab/Skeleton";
 import { Box } from "@material-ui/core";
 import MyCarousel from "react-multi-carousel";
 import DiscountCarousel from "Components/DiscountCarousel";
 import header1 from "Assets/images/header1.jpg";
+import { isEmpty } from "utils/common";
 
 const responsive = {
   desktop: {
@@ -44,7 +44,9 @@ function Home() {
   const classes = useStyles();
   const [socialImages, setSocialImages] = useState([]);
   const [loading, setLoading] = useState(false);
-  const { restaurant } = useRestaurantContext();
+  const {
+    restaurant: { restaurant, placeData },
+  } = useRestaurantContext();
 
   const fetchSocialImages = async () => {
     try {
@@ -64,30 +66,50 @@ function Home() {
   }, []);
 
   const [openingHours, setOpeningHours] = useState([
-    { openDay: "Sunday" },
-    { openDay: "Monday" },
-    { openDay: "Tuesday" },
-    { openDay: "Wednesday" },
-    { openDay: "Thursday" },
-    { openDay: "Friday" },
-    { openDay: "Saturday" },
+    { id: 1, openDay: "Monday" },
+    { id: 2, openDay: "Tuesday" },
+    { id: 3, openDay: "Wednesday" },
+    { id: 4, openDay: "Thursday" },
+    { id: 5, openDay: "Friday" },
+    { id: 6, openDay: "Saturday" },
+    { id: 0, openDay: "Sunday" },
   ]);
+
   useEffect(() => {
-    const fetchGMBLocation = async () => {
-      const res = await getGoogleMyBusinessLocations();
-      setOpeningHours(res?.data?.regularHours?.periods);
-    };
-    fetchGMBLocation();
-  }, []);
+    formatOpeningHours();
+  }, [placeData]);
+
+  const splitTime = (time) => time.slice(0, 2) + ":" + time.slice(2);
+
+  function formatOpeningHours() {
+    if (!isEmpty(placeData)) {
+      const {
+        opening_hours: { periods },
+      } = placeData;
+      for (const { open, close } of periods) {
+        setOpeningHours((prevOpeningHours) =>
+          prevOpeningHours.map((openingHour) =>
+            openingHour?.id === open?.day
+              ? {
+                  ...openingHour,
+                  openTime: splitTime(open?.time),
+                  closeTime: splitTime(close?.time),
+                }
+              : openingHour
+          )
+        );
+      }
+    }
+  }
 
   return (
     <div className={classes.mainDeev}>
       <Navbar />
       <Hero
-        textOne={restaurant?.restaurant?.name ?? "Uncle Sammy"}
-        textTwo={restaurant?.restaurant?.slogan ?? " The real taste is here!"}
+        textOne={restaurant?.name ?? "Uncle Sammy"}
+        textTwo={restaurant?.slogan ?? " The real taste is here!"}
         url={header1}
-        restaurantLogo={restaurant?.restaurant?.logoUrl}
+        restaurantLogo={restaurant?.logoUrl}
       />
       <div className={classes.container}>
         <Card className={classes.root2}>
@@ -118,7 +140,7 @@ function Home() {
           </div>
         </div>
       </div>
-      <Section2 />
+      <Section2 restaurant={restaurant} placeData={placeData} />
 
       <DiscountCarousel />
 
@@ -184,7 +206,12 @@ function Home() {
               {openingHours.map((item) => (
                 <>
                   <div>
-                    <Card className={classes.timingCardStyles}>
+                    <Card
+                      className={`${classes.timingCardStyles} ${
+                        !(item?.openTime || item?.closeTime) &&
+                        classes.closeRestaurant
+                      }`}
+                    >
                       <CardContent className={classes.timingCardContect}>
                         <Typography className={classes.typoStyles4}>
                           {item?.openDay}
@@ -193,10 +220,17 @@ function Home() {
                     </Card>
                   </div>
                   <div>
-                    <Card className={classes.timingCardStyles}>
+                    <Card
+                      className={`${classes.timingCardStyles} ${
+                        !(item?.openTime || item?.closeTime) &&
+                        classes.closeRestaurant
+                      }`}
+                    >
                       <CardContent className={classes.timingCardContect}>
                         <Typography className={classes.typoStyles4}>
-                          {item?.openTime} - {item?.closeTime}
+                          {item?.openTime || item?.closeTime
+                            ? item?.openTime + " - " + item?.closeTime
+                            : "Closed"}
                         </Typography>
                       </CardContent>
                     </Card>
