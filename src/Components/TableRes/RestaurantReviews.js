8505e20cda09d12469e8bfff843125cde9521dbc
naumanmallik <m.nauman@frizhub.com>
2021-09-07 13:11:22 +0500

    UI fixed in reviews carousel

--- src/Components/TableRes/RestaurantReviews.js
+++ src/Components/TableRes/RestaurantReviews.js
@@ -6,11 +6,36 @@ import like from "../../images/like.png";
 import chat from "../../images/chat.png";
 import ImageAvatars from "../Avatar/Avatar";
 import { Carousel } from "react-bootstrap";
+import { Skeleton } from "@material-ui/lab";
+import leftCarouselIcon from "Assets/images/leftCarouselIcon.png";
+import rightCarouselArror from "Assets/images/rightCarouselArror.png";
 
 export default function RestaurantReviews({ restaurant }) {
   const { restaurant: restaurantObj, placeData } = restaurant;
 
   const classes = useStyles();
+
+  const [index, setIndex] = React.useState(0);
+  const [direction, setDirection] = React.useState(null);
+  const [carouselItemCount, setCarouselItemCount] = React.useState(3);
+
+  React.useEffect(() => {
+    setCarouselItemCount(placeData?.reviews?.length);
+  }, [placeData?.reviews]);
+
+  function toggleCarousel(direction) {
+    const [min, max] = [0, carouselItemCount - 1];
+
+    if (direction === "next") {
+      if (index === max) setIndex(0);
+      else setIndex(index + 1);
+    } else if (direction === "prev") {
+      if (index === 0) setIndex(max);
+      else setIndex(index - 1);
+    }
+
+    setDirection(direction);
+  }
 
   return (
     <div className={classes.textDivStyles}>
@@ -46,25 +71,58 @@ export default function RestaurantReviews({ restaurant }) {
           {placeData?.user_ratings_total} REVIEWS
         </div>
         <div className={classes.reviews}>
-          <Carousel indicators={false} interval={1000000}>
-            {placeData?.reviews?.length > 0 &&
+          <Carousel
+            fade
+            indicators={false}
+            controls={false}
+            activeIndex={index}
+            direction={direction}
+          >
+            {placeData?.reviews?.length > 0 ? (
               placeData?.reviews?.map((review) => {
                 return (
                   <Carousel.Item interval={5000}>
-                    <div className={classes.carouselContent}>
-                      <img
-                        src={review?.profile_photo_url}
-                        style={{ width: "300px" }}
-                        className="mb-3"
-                      />
-                      <p className={classes.carouselMessage}>
-                        ‘’{review.text}’’
-                      </p>
-                      <p style={{ color: "#000" }}>-{review?.author_name}-</p>
+                    <div className="d-flex justify-content-between">
+                      <a
+                        className={classes.carouselIcon}
+                        onClick={() => toggleCarousel("prev")}
+                      >
+                        <img
+                          src={leftCarouselIcon}
+                          className={classes.carouselIconWidth}
+                        />
+                      </a>
+
+                      <div className={classes.carouselContent}>
+                        <img
+                          src={review?.profile_photo_url}
+                          style={{ width: "300px" }}
+                          className="mb-3"
+                        />
+                        <p className={classes.carouselMessage}>
+                          ‘’{review.text}’’
+                        </p>
+                        <p className={classes.reviewAuthorName}>
+                          -{review?.author_name}-
+                        </p>
+                      </div>
+
+                      <a
+                        className={classes.carouselIcon}
+                        onClick={() => toggleCarousel("next")}
+                      >
+                        <img
+                          src={rightCarouselArror}
+                          className={classes.carouselIconWidth}
+                        />
+                      </a>
                     </div>
                   </Carousel.Item>
                 );
-              })}
+              })
+            ) : (
+              <Skeleton variant="rect" height={350} />
+            )}
           </Carousel>
         </div>
         {/* <div className={classes.cardContainer}>
