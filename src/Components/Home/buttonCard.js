f6fcdcb0594cc0662beaa18c7dbe8836d1659ac4
se-Haroon <haroon.rasheed@frizhub.com>
2021-09-28 19:14:17 +0500

    fix: fixed menu UI bugs

--- src/Components/Home/buttonCard.js
+++ src/Components/Home/buttonCard.js
@@ -6,7 +6,7 @@ import Typography from "@material-ui/core/Typography";
 const useStyles = makeStyles({
   root: {
     border: "4px solid #10B981",
-    boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+    boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.20)",
     borderRadius: "30px",
     width: "100%",
     cursor: "pointer",
