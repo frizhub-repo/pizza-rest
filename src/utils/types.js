23bf07ef6eadf2475035026853901b64d0f5cc6e
naumanmallik <m.nauman@frizhub.com>
2021-09-30 18:44:46 +0500

    Email format and loading in customer auth

--- src/utils/types.js
+++ src/utils/types.js
@@ -10,3 +10,7 @@ export const GET_PRODUCTS_BY_CATEGORY = "PRODUCTS BY CATEGORY";
 
 // Table reservation types
 export const RESERVE_TABLE = "RESERVE TABLE";
+
+// CONSTANTS
+export const EMAIL_REGEX =
+  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
