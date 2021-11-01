1743e9cabad01998b3ce2ea069c1c3a7ee6734fe
huzaifa1214 <huzaifaimran1998@gmail.com>
2021-11-01 19:54:12 +0500

    stripe: load stripe issue fix

--- src/Hooks/useStripe.js
+++ src/Hooks/useStripe.js
@@ -4,14 +4,17 @@ import * as React from "react";
 
 export default function useStripe() {
   const [stripeAccount, setStripeAccount] = React.useState(null);
+  const [connected, setConnected] = React.useState(false);
   React.useEffect(() => {
     (async () => {
       const res = await getOwnerStripeAccountId();
+      setConnected(true);
       setStripeAccount(res?.data ?? "");
     })();
   }, []);
-  return (
-    stripeAccount &&
-    loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY, { stripeAccount })
-  );
+  return connected
+    ? loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY, {
+        stripeAccount,
+      })
+    : null;
 }
